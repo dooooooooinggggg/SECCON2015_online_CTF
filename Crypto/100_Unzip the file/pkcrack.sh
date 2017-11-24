9776102ee341378e798426fa83f353f5957abcc2
dooooooooinggggg <dooooooooinggggg@gmail.com>
2017-11-24 20:02:39 +0900

    100_Unzip the file done

---
+++ Crypto/100_Unzip the file/pkcrack.sh
@@ -0,0 +1,9 @@
+
+pkcrack -C unzip \
+    -c backnumber08.txt \
+    -c backnumber09.txt \
+    -P backnumber08.zip \
+    -P backnumber09.zip \
+    -p backnumber08.txt \
+    -p backnumber09.txt \
+    -d unzip_output
